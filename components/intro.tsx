10ff436ed05f6dcf8747ed03750e73f18e62d10b
Ubaidullah <121765405+UbaidullahMOmer@users.noreply.github.com>
2023-12-02 02:51:54 -0800

    ghfhg

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
         <span className="font-bold">font-end developer</span> with{" "}
         <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">(React.js)</span>.
+        <span className="underline">React.js</span>.
       </motion.h1>
 
       <motion.div
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/ubaidullahmomer/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/UbaidullahMOmer"
           target="_blank"
         >
           <FaGithubSquare />
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,10 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import Weather_Web_AppImg from "@/public/Weather_Web_App.png";
+import dzFoodImg from "@/public/Dz_food.png";
+import Text_EditorImg from "@/public/Text_Editor.png";
+import CalendarImg from "@/public/Calendar.png";
 
 export const links = [
   {
@@ -62,46 +63,55 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Dz Food",
     description:
-      "I worked as a font-end developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      'I working as a font-end developer on this project for 1 years. Users can give public feedback to companies.',
+    tags: ["HTML", "CSS", "React", "Logical Coding", "JS Functions"],
+    imageUrl: dzFoodImg,
+    url: "https://ubaidullahcalendar.netlify.app/"
   },
   {
-    title: "rmtDev",
+    title: "Shaduler Calendar",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      'I Developed a calendar component I created this calendar from scratch and continually improved its capabilities during my spare time.',
+    tags: ["HTML", "CSS", "React", "Logical Coding", "JS Functions"],
+    imageUrl: CalendarImg,
+    url: "https://ubaidullahcalendar.netlify.app/"
   },
   {
-    title: "Word Analytics",
+    title: "Text Editor",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "I designed and implemented a custom text editor, incorporating a wide range of features and functionalities to enhance the user experience.",
+    tags: ["HTML", "CSS", "React", "Logical Coding", "Js Functions"],
+    imageUrl: Text_EditorImg,
+    url: "https://ubaidullahtexteditor.netlify.app/"
+  },
+  {
+    title: "Weather Web App",
+    description:
+      "I created this Weather Web App. Within this project, users can search for weather information for any city or country, expanding my proficiency in handling API data.",
+    tags: ["HTML", "CSS", "React", "Logical Coding", "API Integreation"],
+    imageUrl: Weather_Web_AppImg,
+    url: "https://ubaidullahweather.netlify.app/"
   },
 ] as const;
 
 export const skillsData = [
   "HTML",
   "CSS",
+  "Bootstrap",
+  "Tailwind",
+  "Meaturial UI",
   "JavaScript",
   "TypeScript",
   "React",
   "Next.js",
   "Node.js",
   "Git",
-  "Tailwind",
-  "Prisma",
+  "Github",
   "MongoDB",
-  "Redux",
+  "Redux Toolkit",
   "GraphQL",
-  "Apollo",
   "Express",
   "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
 ] as const;
